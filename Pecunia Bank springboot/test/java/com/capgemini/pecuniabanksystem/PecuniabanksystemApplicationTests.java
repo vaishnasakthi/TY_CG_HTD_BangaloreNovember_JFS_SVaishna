1d1a8dc4ffee926d1ba9cea688a9a622d6917af5
vaishnasakthi <vaishna1998@gmail.com>
2020-06-10 10:27:22 +0530

    Pecunia Bank Springboot

---
+++ Pecunia Bank springboot/test/java/com/capgemini/pecuniabanksystem/PecuniabanksystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.pecuniabanksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PecuniabanksystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
