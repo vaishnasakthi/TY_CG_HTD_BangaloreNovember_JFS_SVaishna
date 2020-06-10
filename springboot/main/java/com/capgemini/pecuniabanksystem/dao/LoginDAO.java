ea90ce152323d192d89810985ad771af497aac92
vaishnasakthi <vaishna1998@gmail.com>
2020-06-10 11:02:22 +0530

    cg

--- springboot/main/java/com/capgemini/pecuniabanksystem/dao/LoginDAO.java
+++
@@ -1,8 +0,0 @@
-package com.capgemini.pecuniabanksystem.dao;
-
-import com.capgemini.pecuniabanksystem.dto.Login;
-import com.capgemini.pecuniabanksystem.exception.CustomException;
-
-public interface LoginDAO {
-	public Login login(Login login) throws CustomException;
-}
--- springboot/main/java/com/capgemini/pecuniabanksystem/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.capgemini.pecuniabanksystem.service;
-
-import com.capgemini.pecuniabanksystem.dto.Login;
-import com.capgemini.pecuniabanksystem.exception.CustomException;
-
-public interface LoginService {
-public Login login(Login login) throws CustomException; 
-}
